36732e565c2390b8654a425957b56e0c5aa5507f
siddarthyakkala <siddarthyakkala@gmail.com>
2024-05-28 18:21:56 +0530

    Inital commit

---
+++ routes/userRoute.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const {loginController,registerController}=require('../controllers/userController')
+//router object 
+const router = express.Router()
+
+//POST
+
+router.post("/login",loginController);
+
+router.post("/register",registerController);
+
+module.exports=router
---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const cors=require('cors')
+const morgan=require('morgan')
+const dotenv=require('dotenv')
+const colors=require('colors')
+const connectDb = require('./config/connectDb')
+dotenv.config()
+
+connectDb();
+const app=express()
+
+//middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('<h1> Hello from server <h1>')
+})
+
+const PORT=8080 || process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+
